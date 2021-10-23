689dfff2b4193c2e74266f4eefadb44abed90c6a
Ivan Martinez <stantcom93@gmail.com>
2021-10-23 18:20:02 +0200

    activity extensions

--- app/src/main/java/com/example/conncurso_de_tartas/core/ex/ActivityExtensions.kt
+++ app/src/main/java/com/example/conncurso_de_tartas/core/ex/ActivityExtensions.kt
@@ -1,2 +1,8 @@
 package com.example.conncurso_de_tartas.core.ex
 
+import android.app.Activity
+import android.widget.Toast
+
+fun Activity.showLongToast(message: String){
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
