cb417915c3ee62edbbef99f8ed521397fdc480bb
Ivan Martinez <stantcom93@gmail.com>
2021-10-23 18:22:06 +0200

    extension functions implemented and code cleaned

--- app/src/main/java/com/example/conncurso_de_tartas/view/MainActivity.java
+++ app/src/main/java/com/example/conncurso_de_tartas/view/MainActivity.java
@@ -1,17 +1,20 @@
-package com.example.conncurso_de_tartas;
+package com.example.conncurso_de_tartas.view;
 
-import static com.example.conncurso_de_tartas.Constants.ANIMATION_LENGTH_TIME;
+
+
+import static com.example.conncurso_de_tartas.core.constants.Constants.ANIMATION_LENGTH_TIME;
+import static com.example.conncurso_de_tartas.core.ex.ActivityExtensionsKt.showLongToast;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.example.conncurso_de_tartas.R;
 import com.example.conncurso_de_tartas.databinding.ActivityMainBinding;
 
 import java.util.Objects;
@@ -20,23 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
 
     //Declaramos las variables
-    private ActivityMainBinding binding; //Utilizamos una vinculacion directa al xml qeu sera mas comodo y mas eficiente
+    private ActivityMainBinding binding; //Utilizamos una vinculacion directa al xml que es comodo y eficiente
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  // Funcion oncreate desde aqui se crea la actividad
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setUpLayout();
         setUpListeners();
     }
 
-    private void setUpListeners() {
-        binding.btnSubmit.setOnClickListener(view -> initSubmitLogic());
+    private void setUpLayout() {  // Inflamos la vista desde la clase autogenerada activitimain
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 
-        binding.cbHasParticipated.setOnCheckedChangeListener((compoundButton, b) -> {
+    /*
+    Clase para el escuchador del boton SUBMIT y la animacion del EditText una vez el chech box se
+    selecione y deselecione
+     */
+
+    private void setUpListeners() {
+        binding.btnSubmit.setOnClickListener(view -> initSubmitLogic());//view binding de boton submit con funcion lamda llamando a la clase
+
+        binding.cbHasParticipated.setOnCheckedChangeListener((compoundButton, b) -> { //Logica de la animacion del Checkbox
             if (compoundButton.isChecked()) {
                 YoYo.with(Techniques.SlideInDown).duration(ANIMATION_LENGTH_TIME).playOn(binding.tilHasParticipated);
                 binding.tilHasParticipated.setVisibility(View.VISIBLE);
@@ -50,6 +60,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*
+    Logica en la cual se leen todos los datos nombre,apellido , edad,  sexo etc..
+    -comprueba si todos los campos estan rellenados con los valores que necesitan.
+    -Comprueba si es mayor de 17 años
+    -Comprueba si el EditText ha puesto otro concurso de tartas
+    -Si los datos son incoorectos se printea unos Toast especificos por cada error
+     */
+
     private void initSubmitLogic() {
         if (isNameOrSurnameValid(Objects.requireNonNull(binding.tilName.getEditText()).getText().toString()) &&
                 isNameOrSurnameValid(Objects.requireNonNull(binding.tilSurname.getEditText()).getText().toString()) &&
@@ -57,30 +75,33 @@ public class MainActivity extends AppCompatActivity {
             if (isAgeValid(Objects.requireNonNull(binding.tilAge.getEditText()).getText().toString())) {
                 if (binding.cbHasParticipated.isChecked()) {
                     if (Objects.requireNonNull(binding.tilHasParticipated.getEditText()).getText().toString().isEmpty()) {
-                        showToast(getString(R.string.old_contest_name));
+                        showLongToast(this, getString(R.string.old_contest_name));
                     } else {
-                        showToast(getString(R.string.registration_completed));
+                        showLongToast(this, getString(R.string.registration_completed));
                     }
 
                 } else {
-                    showToast(getString(R.string.registration_completed));
+                    showLongToast(this, getString(R.string.registration_completed));
                 }
 
             } else {
-                showToast(getString(R.string.make_sure_you_are_older_18));
+                showLongToast(this, getString(R.string.make_sure_you_are_older_18));
             }
 
         } else {
-            showToast(getString(R.string.make_sure_your_data_is_not_empty));
+            showLongToast(this, getString(R.string.make_sure_your_data_is_not_empty));
+            /*
+            He extraido en values/strings todos los texxtos printeados y los exporto con R.string para que no este hardcodeado
+             */
         }
     }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
 
 
-    //Validators
+
+    //Validators : estos comprueban quer no esten vacios los EditText y que la edad sea la correcta
+
+
     private Boolean isNameOrSurnameValid(String name) {
         try {
             if (Integer.parseInt(name) >= 0) {
